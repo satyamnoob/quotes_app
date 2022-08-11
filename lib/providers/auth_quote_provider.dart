1a7d2a1eb055e53c39c2e5577727a549416bfd5c
Satyam Shukla <61821492+satyamnoob@users.noreply.github.com>
2022-08-11 15:09:58 +0530

    Update auth_quote_provider.dart
    le chus

--- lib/providers/auth_quote_provider.dart
+++ lib/providers/auth_quote_provider.dart
@@ -1,21 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quotes_app/models/author_quote.dart';
 
-class AuthQuoteProvider extends StateNotifier<AuthorQuote?> {
-  AuthorQuote? quote;
-
-  AuthQuoteProvider() : super(null);
-
-  void addQuote(AuthorQuote? quote) {
-    state = quote;
-  }
-
-  void deleteQuote() {
-    state = null;
-  }
-}
-
-final authQuoteProvider = StateNotifierProvider<AuthQuoteProvider, AuthorQuote?>(
-  (ref) => AuthQuoteProvider(),
-);
+final StateProvider authQuoteProvider = StateProvider<AuthorQuote?>((ref) => null);
